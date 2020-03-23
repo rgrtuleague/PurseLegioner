a78092a81fedfac5a07199564d5463f2095defb9
SS <rgrtuleague@gmail.com>
2020-03-23 13:17:47 +0300

    Добавлены кнопки ПОПОЛНИТЬ и ПОТРАТИТЬ с соответствующими обработчика события - клика, по которому изменятеся значение текущего баланса

--- app/src/main/java/com/example/purselegioner/MainActivity.kt
+++ app/src/main/java/com/example/purselegioner/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.purselegioner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addBalance(buttonBalancePlus, 10.00)
+        minusBalance(buttonBalanceMinus, 15.00)
+    }
+
+    private fun addBalance(button: Button, addValue: Double) {
+        button.setOnClickListener() {
+            var number = currentBalanceText.text.toString().toDouble()
+            currentBalanceText.text = (number + addValue).toString()
+        }
+    }
+
+    private fun minusBalance(button: Button, minusValue: Double) {
+        button.setOnClickListener() {
+            var number = currentBalanceText.text.toString().toDouble()
+            currentBalanceText.text = (number - minusValue).toString()
+        }
     }
 }
+
