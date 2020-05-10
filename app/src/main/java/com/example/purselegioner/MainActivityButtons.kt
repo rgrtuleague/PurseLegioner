03400ac6cc6085ee5ae4c8adf887991e24973295
Sergey Shmyrenkov <rgrtuleague@gmail.com>
2020-05-10 15:31:21 +0300

    add link to article COROUTINES

--- app/src/main/java/com/example/purselegioner/MainActivityButtons.kt
+++ app/src/main/java/com/example/purselegioner/MainActivityButtons.kt
@@ -1,18 +1,22 @@
 package com.example.purselegioner
 
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
- fun addBalance(button : Button, currentBalanceText : TextView, inputBalanceChange : EditText) {
+fun addBalance(button: Button, currentBalanceText: TextView, inputBalanceChange: View) {
+
     button.setOnClickListener() {
         val currentBalance = currentBalanceText.text.toString().format("%.2f", it).toDouble()  // текущее значение баланса
-        if (!inputBalanceChange.text.isNullOrEmpty()) {
-            val value = inputBalanceChange.text.toString().format("%.2f", it)
+        //val currentBalance = currentBalanceText.getDoubleTypeValueTextView() // не работает.
+//        if (!inputBalanceChange.text.isNullOrEmpty()) {
+        if (!inputBalanceChange.toString().isEmpty()) {
+//            val value = inputBalanceChange.text.toString().format("%.2f", it)
+            val value = inputBalanceChange.toString().format("%.2f", it)
             val regex = "^\\d+(\\.?\\d{1,2}){0,1}\$".toRegex()
             if (regex.matches(value)) {
-                val inputValueChange = inputBalanceChange.text.toString().toDouble()
+//                val inputValueChange = inputBalanceChange.text.toString().toDouble()
+                val inputValueChange = inputBalanceChange.toString().toDouble()
                 val result : String = String.format("%.2f", (currentBalance + inputValueChange))
                 currentBalanceText.text = result
             }
@@ -20,15 +24,18 @@ import kotlinx.android.synthetic.main.activity_main.*
     }
 }
 
-fun minusBalance(button : Button, currentBalanceText : TextView, inputBalanceChange : EditText) {
+fun minusBalance(button: Button, currentBalanceText: TextView, inputBalanceChange: View) {
     button.setOnClickListener() {
         val currentBalance = currentBalanceText.text.toString().format("%.2f", it).toDouble()  // текущее значение баланса
         //val currentBalance = currentBalanceText.getDoubleTypeValueTextView() // не работает.
-        if (!inputBalanceChange.text.isNullOrEmpty()) {
-            val value = inputBalanceChange.text.toString().format("%.2f", it)
+//        if (!inputBalanceChange.text.isNullOrEmpty()) {
+        if (!inputBalanceChange.toString().isEmpty()) {
+//            val value = inputBalanceChange.text.toString().format("%.2f", it)
+           val value = inputBalanceChange.toString().format("%.2f", it)
             val regex = "^\\d+(\\.?\\d{1,2}){0,1}\$".toRegex()
             if (regex.matches(value)) {
-                val inputValueChange = inputBalanceChange.text.toString().toDouble()
+//                val inputValueChange = inputBalanceChange.text.toString().toDouble()
+                val inputValueChange = inputBalanceChange.toString().toDouble()
                 val result : String = String.format("%.2f", (currentBalance - inputValueChange))
                 currentBalanceText.text = result
             }
